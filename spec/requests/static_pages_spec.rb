11ba3544b6549e85feddc690ea44f74451140877
Jerry Bruno <mr_orange27@hotmail.com>
2012-03-04 18:56:23 -0500

    contact start

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,12 +18,12 @@ describe "Static pages" do
 describe "Help page" do
 	
 	it "should have the h1 'Help'" do
-			visit '/static_pages/home'
+			visit '/static_pages/help'
 			page.should have_selector('h1' , :text=> 'Help')
 		end
 		
 		it "should have the title 'Help'" do
-			visit '/static_pages/home'
+			visit '/static_pages/help'
 			page.should have_selector('title' , :text=> "Ruby on Rails Tutorial Sample App | Help" )
 		end
 	end
@@ -31,15 +31,27 @@ describe "Help page" do
 describe "About page" do
 
 		it "should have the h1 'About'" do
-			visit '/static_pages/home'
+			visit '/static_pages/about'
 			page.should have_selector('h1' , :text=> 'About Us')
 		end
 		
 		it "should have the title 'About Us'" do
-			visit '/static_pages/home'
+			visit '/static_pages/about'
 			page.should have_selector('title' , :text=> "Ruby on Rails Tutorial Sample App | About Us" )
 		end
 	end
+describe "Contacts" do 
+
+	it "should have the h1 'Contacts'" do
+	visit '/static_pages/contacts'
+	page.should have_selector('h1' , :text=> 'Contacts')
+	end
+	
+	it "should have the title 'Contacts'" do
+	visit 'static_pages/contacts'
+	page.should have_selector('title' , :text=> "Ruby on Rails Tutorial Sample APP | Contacts" )
+	end
+end
 end
 
 	
